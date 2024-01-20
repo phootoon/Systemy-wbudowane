e04fd0575d020e59c19126adb7f43c101282345e
phootoon <M.rozbicki1995@gmail.com>
2024-01-20 16:39:28 +0100

    Update karmink.py

--- karmink.py
+++ karmink.py
@@ -1,34 +1,61 @@
 import RPi.GPIO as GPIO
+import os
 import time
+from gpiozero import Servo
+from time import sleep
 
-# Set the GPIO mode
+# GPIO setup
+buttonPin = 27
+servoPin = 17
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Set the GPIO pin (for example, 17) to which the servo is connected
-servo_pin = 17
-GPIO.setup(servo_pin, GPIO.OUT)
+# Servo setup
+servo = Servo(servoPin)
 
-# Set the frequency of the PWM signal
-pwm = GPIO.PWM(servo_pin, 50)
-pwm.start(0)
+# Constants for the button debounce and double click detection
+CLICK_DELAY = 0.5  # 0.5 seconds to detect double click
+ROTATION_ANGLE = 1/8  # 1/8 rotation
 
-def set_servo_angle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(servo_pin, True)
-    pwm.ChangeDutyCycle(duty)
-    time.sleep(1)
-    GPIO.output(servo_pin, False)
-    pwm.ChangeDutyCycle(0)
+# Initial state
+lastButtonState = False
+lastDebounceTime = 0
+clickCount = 0
+
+def rotate_servo():
+    initial_position = servo.value
+    target_position = initial_position + ROTATION_ANGLE
+    if target_position > 1:  # Ensure we don't exceed the servo's range
+        target_position = 1
+    servo.value = target_position
+    sleep(0.5)
+    servo.value = initial_position  # Reset to initial position
 
 try:
     while True:
-        # Rotate the servo to 0 degrees
-        set_servo_angle(0)
-        time.sleep(1)
-        # Rotate the servo to 180 degrees
-        set_servo_angle(180)
-        time.sleep(1)
+        currentButtonState = GPIO.input(buttonPin)
+        
+        # Check for button state change
+        if currentButtonState != lastButtonState:
+            lastDebounceTime = time.time()
 
-except KeyboardInterrupt:
-    pwm.stop()
+        if (time.time() - lastDebounceTime) > CLICK_DELAY:
+            # Button clicked once
+            if clickCount == 1:
+                rotate_servo()
+            
+            # Button double clicked
+            elif clickCount == 2:
+                os.system("sudo shutdown -h now")
+            
+            clickCount = 0
+
+        # Update the number of clicks
+        if currentButtonState == False and lastButtonState == True:
+            clickCount += 1
+
+        lastButtonState = currentButtonState
+        time.sleep(0.01)
+
+finally:
     GPIO.cleanup()
